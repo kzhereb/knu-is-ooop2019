5a879fe605d8737118886e21da2945c31be5277d
Kostiantyn Zhereb <zhereb@gmail.com>
2019-09-16 15:41:16 +0300

    Initial code for doubly linked list (from previous year Programming course)

--- unit-testing/catch_demo_k28lesson2.cpp
+++ unit-testing/catch_demo_k28lesson2.cpp
@@ -5,7 +5,7 @@
  *      Author: KZ
  */
 
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+//#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
 
