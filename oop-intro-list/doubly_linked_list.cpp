a69665afaa0acff450e6012452d5c24dd593f4c5
Kostiantyn Zhereb <zhereb@gmail.com>
2019-10-01 08:40:27 +0300

    Moving some classes into separate header file
    This is not entirely correct: if this header is included in multiple
    compilation units, duplicate methods will appear

--- oop-intro-list/doubly_linked_list.cpp
+++ oop-intro-list/doubly_linked_list.cpp
@@ -5,6 +5,8 @@
  *      Author: KZ
  */
 
+#include "policies.h"
+
 #include <iostream>
 #include <vector>
 using std::cout;
@@ -284,51 +286,6 @@ public:
 
 };
 
-class Policy {
-public:
-	virtual bool change_capacity(int size, int& capacity) =0;
-};
-class GrowPolicy: public Policy {
-public:
-	bool change_capacity(int size, int& capacity) override ;
-};
-bool GrowPolicy::change_capacity(int size, int& capacity) {
-	if (size == capacity) {
-		capacity *=2;
-		return true;
-	}
-	return false;
-}
-
-class GrowTriplePolicy: public GrowPolicy {
-public:
-	bool change_capacity(int size, int& capacity) override {
-		if (size == capacity) {
-			capacity *=3;
-			return true;
-		}
-		return false;
-	}
-};
-
-class ShrinkPolicy: public Policy {
-public:
-	bool change_capacity(int size, int& capacity) override {
-		// default shrink policy is to never shrink
-		return false;
-	}
-};
-
-class ShrinkQuarterPolicy: public ShrinkPolicy {
-public:
-	bool change_capacity(int size, int& capacity) override {
-		if (size*4<capacity) {
-			capacity = capacity / 2;
-			return true;
-		}
-		return false;
-	}
-};
 
 template<typename T>
 T get_test_data(int index) {
