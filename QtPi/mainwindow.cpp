1dda5f8063b8f0ddec3e78b43c0cbde2c06fdac6
Kostiantyn Zhereb <zhereb@gmail.com>
2019-10-22 11:16:37 +0300

    Radio buttons to select calculator

--- QtPi/mainwindow.cpp
+++ QtPi/mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "picalculator.h"
+
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +15,18 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_rbAtan_clicked()
+{
+    AtanCalculator calc;
+    double result = calc.calculate(1000);
+    qDebug()<<"Atan "<<result;
+}
+
+void MainWindow::on_rbIntegrate_clicked()
+{
+    IntegrateCalculator calc;
+    double result = calc.calculate(1000);
+    qDebug()<<"Integrate "<<result;
 }
