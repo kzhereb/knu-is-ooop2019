3f6abde59d97518d867acbca3d9880d4dbf6e241
Kostiantyn Zhereb <zhereb@gmail.com>
2020-03-06 12:02:21 +0200

    Moved ArrayListConfigurable into a separate header

--- oop-intro-list/doubly_linked_list.cpp
+++ oop-intro-list/doubly_linked_list.cpp
@@ -7,6 +7,7 @@
 #include "list.h"
 #include "helper.h"
 #include "array_list.h"
+#include "array_list_configurable.h"
 #include "doubly_linked_list.h"
 #include "policies.h"
 
@@ -17,61 +18,6 @@ using std::cout;
 using std::endl;
 
 
-
-
-
-template<typename T, typename GrowPolicyT, typename ShrinkPolicyT>
-class ArrayListConfigurable: public ArrayList<T> {
-protected:
-	GrowPolicyT* grow_policy;
-	ShrinkPolicyT* shrink_policy;
-	void grow_capacity() override {
-		bool changed = grow_policy->change_capacity(this->size, this->capacity);
-		if (!changed) {return;}
-		T* new_items = new T[this->capacity];
-		for (int i = 0; i < this->size; i++) {
-			new_items[i] = this->items[i];
-		}
-		delete[] this->items;
-		this->items = new_items;
-	}
-	void shrink_capacity() {
-		bool changed = shrink_policy->change_capacity(this->size, this->capacity);
-		if (!changed) {return;}
-		T* new_items = new T[this->capacity];
-		for (int i = 0; i < this->size; i++) {
-			new_items[i] = this->items[i];
-		}
-		delete[] this->items;
-		this->items = new_items;
-	}
-public:
-	ArrayListConfigurable(T first_data, GrowPolicyT* grow_policy = nullptr,
-			ShrinkPolicyT* shrink_policy = nullptr) :
-			ArrayList<T> { first_data }, grow_policy { grow_policy }, shrink_policy {
-					shrink_policy } {
-		if (grow_policy == nullptr) {
-			this->grow_policy = new GrowPolicyT;
-		}
-		if (shrink_policy == nullptr) {
-			this->shrink_policy = new ShrinkPolicyT;
-		}
-	}
-
-	bool remove(T key) override {
-		int key_index = this->find(key);
-		if (key_index == -1) { // not found
-			return false;
-		}
-		shrink_capacity();
-		for (int i = key_index; i < this->size - 1; i++) {
-			this->items[i] = this->items[i + 1];
-		}
-		this->size--;
-		return true;
-	}
-
-};
 
 
 template<typename T>
