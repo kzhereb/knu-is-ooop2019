095e785e0e67f83fdbcd0eac10b85456322d6ed0
Kostiantyn Zhereb <zhereb@gmail.com>
2020-03-06 13:38:28 +0200

    Example of Adapter pattern
    Adapts std::vector to custom List<T> interface

---
+++ design-patterns/adapter.cpp
@@ -0,0 +1,33 @@
+/*
+ * adapter.cpp
+ *
+ *  Created on: Mar 6, 2020
+ *      Author: KZ
+ */
+
+#include "../unit-testing/catch.hpp"
+#include "../oop-intro-list/list.h"
+#include "../oop-intro-list/array_list.h"
+#include "../oop-intro-list/vector_list.h"
+#include <string>
+#include <sstream>
+
+
+TEST_CASE("Inserting into list","[patterns]") {
+	List<int>* list = nullptr;
+	SECTION("Array List") {
+		list = new ArrayList<int>{5};
+	}
+	SECTION("Vector List") {
+		list = new VectorList<int>{5};
+	}
+
+	list->insert(5,7);
+
+	std::stringstream out;
+	list->print(out);
+	REQUIRE(out.str() == "5 7 \n");
+
+}
+
+
---
+++ oop-intro-list/vector_list.h
@@ -0,0 +1,53 @@
+/*
+ * vector_list.h
+ *
+ *  Created on: Mar 6, 2020
+ *      Author: KZ
+ */
+
+#ifndef OOP_INTRO_LIST_VECTOR_LIST_H_
+#define OOP_INTRO_LIST_VECTOR_LIST_H_
+
+#include "list.h"
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using std::cout;
+using std::endl;
+
+template<typename T>
+class VectorList: public List<T> {
+private:
+	std::vector<T> impl;
+public:
+	VectorList(T first_data) : impl{first_data} {
+
+	}
+	void print(std::ostream& out = std::cout) override {
+		for(auto const& item: impl) {
+			out << item << " ";
+		}
+		out << endl;
+	}
+	void add(T data) override {
+		impl.push_back(data);
+	}
+	bool insert(T key, T data) override {
+		auto it = std::find(std::begin(impl), std::end(impl), key);
+		if (it == std::end(impl)) {
+			return false;
+		}
+		// our insert() inserts after key; vector insert() inserts before it
+		impl.insert(it+1,data);
+	}
+	bool remove(T key) override {
+		//TODO: implement me
+		throw std::logic_error("Not implemented");
+	}
+};
+
+
+
+
+#endif /* OOP_INTRO_LIST_VECTOR_LIST_H_ */
