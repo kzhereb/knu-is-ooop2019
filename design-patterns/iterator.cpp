7952c777ccb9aa432a695856a021bc3687e0fdc2
Kostiantyn Zhereb <zhereb@gmail.com>
2020-04-23 13:29:52 +0300

    Simple example of Iterator pattern
    Iterate array, skipping items

---
+++ design-patterns/iterator.cpp
@@ -0,0 +1,76 @@
+/*
+ * iterator.cpp
+ *
+ *  Created on: Apr 23, 2020
+ *      Author: KZ
+ */
+#include "../unit-testing/catch.hpp"
+
+#include <iostream>
+#include <cstddef>
+
+class Container {
+
+	class ContainerIterator {
+	private:
+		std::size_t current_position;
+		Container& container;
+	public:
+		ContainerIterator(std::size_t current_position, Container& container):
+			current_position{current_position}, container{container} {}
+
+		ContainerIterator& operator++() {
+			current_position +=2;
+			return *this;
+		}
+		int& operator*() {
+			return container.array[current_position];
+		}
+		bool operator!=(const ContainerIterator& other) {
+			if ((current_position>=container.size) && (other.current_position>=container.size)) {
+				return false;
+			}
+			return (current_position != other.current_position);
+		}
+	};
+
+private:
+	int* array;
+	std::size_t size;
+public:
+	Container(std::size_t size):size{size} {
+		array = new int[size];
+		for(std::size_t i=0;i<size;i++) {
+			array[i] = i;
+		}
+	}
+	~Container() {
+		delete [] array;
+	}
+	ContainerIterator begin() {
+		return ContainerIterator{0,*this};
+	}
+	ContainerIterator odd_begin() {
+		return ContainerIterator{1,*this};
+	}
+	ContainerIterator end() {
+		return ContainerIterator{size,*this};
+	}
+};
+
+TEST_CASE("traversing using iterator","[patterns]") {
+	Container array{10};
+	std::size_t count = 0;
+	for(auto& item:array) {
+		std::cout<<item<<std::endl;
+		REQUIRE(item==count*2);
+		count++;
+	}
+	REQUIRE(count==5);
+}
+
+
+
+
+
+
