ceb9409d4c37d3647f42483ceafcf77cbf73925a
Kostiantyn Zhereb <zhereb@gmail.com>
2020-05-29 11:08:22 +0300

    Implemented more efficient locking
    Now threaded version is faster than sequential

--- multithreaded/create_threads.cpp
+++ multithreaded/create_threads.cpp
@@ -31,17 +31,29 @@ void single_thread(int* arr, std::size_t start, std::size_t end, long long & res
 	}
 }
 
-long long sum_threads(int* arr, std::size_t size) {
+void single_thread_less_locks(int* arr, std::size_t start, std::size_t end, long long & result) {
+	long long local_result = 0;
+	for (std::size_t i = start; i < end; i++) {
+
+		local_result += arr[i];
+	}
+
+	{
+		std::lock_guard<std::mutex> lock(g_mutex);
+		result+=local_result;
+	}
+
+
+}
+template<typename Func>
+long long sum_threads(int* arr, std::size_t size, Func thread_func) {
 	long long result = 0;
 	unsigned num_threads = std::thread::hardware_concurrency();
 	std::vector<std::thread> threads(num_threads);
 	std::size_t thread_size = (size / num_threads)+1;
 	std::size_t start = 0;
 	for (unsigned i = 0; i < num_threads; i++) {
-		//std::thread worker(single_thread,arr,
-		//	start, std::min(start + thread_size, size),result);
-
-		threads.at(i)=std::thread(single_thread, arr,
+		threads.at(i)=std::thread(thread_func, arr,
 			start, std::min(start + thread_size, size), std::ref(result));
 		start += thread_size;
 	}
@@ -68,7 +80,10 @@ TEST_CASE("sum of array elements", "[threads]") {
 		sum = sum_sequential(test_array, size);
 	}
 	SECTION("threads") {
-		sum = sum_threads(test_array, size);
+		sum = sum_threads(test_array, size, single_thread);
+	}
+	SECTION("threads less locks") {
+		sum = sum_threads(test_array, size, single_thread_less_locks);
 	}
 
 	REQUIRE(sum == 340);
@@ -77,18 +92,14 @@ TEST_CASE("sum of array elements", "[threads]") {
 }
 
 TEST_CASE("sum of sequential numbers", "[threads]") {
-	std::size_t size = 1e4;
+	std::size_t size = 1e6;
 	int* test_array = generate_range(size);
 	long long expected_sum = (long long)size * (size - 1) / 2;
 
 	long long sum;
-	BENCHMARK("sequential") {
-		return sum_sequential(test_array, size);
-	};
 
-	BENCHMARK("threads") {
-		return sum_threads(test_array, size);
-	};
+
+
 
 	SECTION("sequential") {
 		auto start = std::chrono::high_resolution_clock::now();
@@ -96,13 +107,33 @@ TEST_CASE("sum of sequential numbers", "[threads]") {
 		auto end = std::chrono::high_resolution_clock::now();
 		std::chrono::duration<double> diff = end - start;
 		std::cout << "sequential time: " << diff.count() << " s" << std::endl;
+
+		BENCHMARK("sequential") {
+			return sum_sequential(test_array, size);
+		};
 	}
 	SECTION("threads") {
 		auto start = std::chrono::high_resolution_clock::now();
-		sum = sum_threads(test_array, size);
+		sum = sum_threads(test_array, size, single_thread);
 		auto end = std::chrono::high_resolution_clock::now();
 		std::chrono::duration<double> diff = end - start;
 		std::cout << "threads time: " << diff.count() << " s" << std::endl;
+
+//		BENCHMARK("threads") {
+//			return sum_threads(test_array, size);
+//		};
+	}
+
+	SECTION("threads less locks") {
+		auto start = std::chrono::high_resolution_clock::now();
+		sum = sum_threads(test_array, size, single_thread_less_locks);
+		auto end = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> diff = end - start;
+		std::cout << "threads time: " << diff.count() << " s" << std::endl;
+
+		BENCHMARK("threads less locks") {
+			return sum_threads(test_array, size, single_thread_less_locks);
+		};
 	}
 
 	REQUIRE(sum == expected_sum);
